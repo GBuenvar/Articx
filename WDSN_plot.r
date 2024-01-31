d970158458509c77a98ca0f90dd65379b42e62ae
GBuenvar <gbuenvaron96@gmail.com>
2024-01-31 14:30:49 +0100

    reciprocal edges added

--- WDSN_plot.r
+++ WDSN_plot.r
@@ -5,8 +5,8 @@ library(circlize)
 mat = matrix(c(0,1,2, 2,0,-1, -1,1,0), nrow = 3, ncol = 3, byrow = TRUE)
 
 # create a random matrix of size 10x10 that contains integers between -5 and 5 and 0 on the diagonal
-# mat = matrix(sample(c(-5:5,0), 100, replace = TRUE), nrow = 10, ncol = 10)
-# diag(mat) = 0
+mat = matrix(sample(c(-5:5,0), 100, replace = TRUE), nrow = 10, ncol = 10)
+diag(mat) = 0
 
 # create a vector of integers from 1 to the number of rows of the matrix
 items = 1:nrow(mat)
@@ -42,6 +42,11 @@ circos.initialize(factors = df$factor, x = df$x)
 # Create a vector of N distinct colors (N = number of rows of the matrix) using pastel colors
 cols = rainbow(nrow(mat), s = 1, v = 1, alpha = 0.4)
 
+red = rgb(1,0,0,0.5)
+blue = rgb(0,0,1,0.5)
+pink = rgb(245/255, 40/255, 145/255, 0.8)
+green = rgb(39/255, 245/255, 39/255, 0.8)
+
 # Build the regions of track #1
 circos.trackPlotRegion(factors = df$factor, ylim = c(0,1) ,bg.col = cols, bg.border = NA, 
     )
@@ -58,13 +63,23 @@ for (i in 1:nrow(mat)){
         j0 = data_start_end$start[j] # start of the arc at the target
         width_ij = abs(mat[i,j])
         width_ji = abs(mat[j,i])
-        # color is red for positive values, blue for negative values, with some transparency
-        col_ij = ifelse(mat[i,j] > 0, rgb(1,0,0,0.5), rgb(0,0,1,0.5))
-        col_ji = ifelse(mat[j,i] > 0, rgb(1,0,0,0.5), rgb(0,0,1,0.5))
 
-        # draw the arc if the value is not 0
-        if (width_ij > 0) circos.link(i, c(i0, i0+width_ij), j, c(j0+width_ji, j0+width_ji), col = col_ij, lwd = 1, lty = 1, border="black" )
-        if (width_ji > 0) circos.link(i, c(i0+width_ij, i0+width_ij), j, c(j0, j0+width_ji), col = col_ji, lwd = 1, lty = 1, border="black" )
+
+        # color is red for positive values, blue for negative values, black for 0
+        col_ij = ifelse(mat[i,j] > 0, red, ifelse(mat[i,j] < 0, blue, "black"))
+        col_ji = ifelse(mat[j,i] > 0, red, ifelse(mat[j,i] < 0, blue, "black"))
+
+        if (col_ij == col_ji & col_ij == red) {
+            col_ij = col_ji = pink
+            circos.link(i, c(i0, i0+width_ij), j, c(j0, j0+width_ji), col = col_ij, lwd = 1, lty = 1, border="black" )
+        } else if (col_ij == col_ji & col_ij == blue) {
+            col_ij = col_ji = green
+            circos.link(i, c(i0, i0+width_ij), j, c(j0, j0+width_ji), col = col_ij, lwd = 1, lty = 1, border="black" )
+        } else {
+            # draw the arc if the value is not 0
+            if (width_ij > 0) circos.link(i, c(i0, i0+width_ij), j, c(j0+width_ji, j0+width_ji), col = col_ij, lwd = 1, lty = 1, border="black" )
+            if (width_ji > 0) circos.link(i, c(i0+width_ij, i0+width_ij), j, c(j0, j0+width_ji), col = col_ji, lwd = 1, lty = 1, border="black" )
+        }
         
         # update the start of the arc at the target
         data_start_end$start[j] = j0 + width_ji
@@ -73,5 +88,5 @@ for (i in 1:nrow(mat)){
     data_start_end$start[i] = i0
 }
 
-legend("bottomleft", legend = c("positive", "negative"), col = c(rgb(1,0,0,0.5), rgb(0,0,1,0.5)), lty = 1, lwd = 1, bty = "n", cex = 1.5)
+legend("bottomleft", legend = c("+", "-", "++", "--"), col = c(red, blue, pink, green), lty = 1, lwd = 1, bty = "n", cex = 1.5)
 circos.clear()
